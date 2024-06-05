27576c86497eb604f8d502bad2f0aeccb1625f6a
patilmanoj19 <patilmanojkumar12@gmail.com>
2024-06-05 14:59:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Display a simple paragraph
+st.write("This is a simple Streamlit app that prints a single paragraph of text. "
+         "Streamlit makes it easy to create web apps for machine learning and data science projects. "
+         "You can write your content in plain text, use Markdown for formatting, "
+         "and easily display various types of information and interactive widgets.")
+
+
+st.text("This is a simple Streamlit app that prints a single paragraph of text.Streamlit makes it easy to create web apps for machine learning and data science projects.You can write your content in plain text, use Markdown for formatting, and easily display various types of information and interactive widgets.")
